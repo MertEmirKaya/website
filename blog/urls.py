7b939cc2ea45755365bcbb1f0d8a7e39aaf8702d
Mert Kaya <mertemir_54@outlook.com>
2022-07-23 17:54:53 +0300

    added searchbar to navbar

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import blog ,blog_detail
+from .views import blog ,blog_detail,searchBar
 urlpatterns = [
     path("",blog,name="blog"),
-    path("<int:pk>",blog_detail,name='blog_detail')
+    path("<int:pk>",blog_detail,name='blog_detail'),
+    path('search',searchBar,name='search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from blog.models import Article
-
+from django.db.models import Q
 # Create your views here.
 
 def blog(request):
@@ -12,3 +12,15 @@ def blog_detail(request,pk):
     article= get_object_or_404(Article, id=pk)
 
     return render(request,'blog/blog-detail.html',{'article':article})
+
+
+def searchBar(request):
+    
+    if request.method=='POST':
+        searched=request.POST['searched']
+        articles=Article.objects.filter(Q(title__contains=searched)|Q(subject__contains=searched)|Q(card_headline__contains=searched))
+        return render(request,'blog/blog.html',{'articles':articles,'searched':searched})
+
+    else:    
+        pass
+        # return render(request,'blog/blog-detail.html',{'articles':articles})  
