3b7ef07ddfe49351d0848e3d5bcb889ffe852ced
Mert Kaya <mertemir_54@outlook.com>
2022-08-05 00:25:43 +0300

    added pagination to blog page and deleted the other articles section from article detail page

--- blog/views.py
+++ blog/views.py
@@ -17,11 +17,14 @@ def blog(request):
 def articles_by_Category(request,category_slug):
     articles=Article.objects.filter(category__slug=category_slug)
     categories=Category.objects.all()
-    return render(request,'blog/blog.html',{'articles':articles,'categories':categories})
+    p=Paginator(articles,4)
+    page=request.GET.get('page')    
+    articlesp=p.get_page(page)
+    return render(request,'blog/blog.html',{'articles':articles,'categories':categories,'articlesp':articlesp})
 
 def blog_detail(request,pk,category_slug):
     article= get_object_or_404(Article, id=pk,category__slug=category_slug)
-    articles=Article.objects.all()[0:7]
+    articles=Article.objects.all()[0:4]
     categories=Category.objects.all()
     tags=Tag.objects.all()
     
@@ -29,11 +32,15 @@ def blog_detail(request,pk,category_slug):
 
 
 def searchBar(request):
+    articles=Article.objects.all()
     categories=Category.objects.all()
+    p=Paginator(articles,4)
+    page=request.GET.get('page')    
+    articlesp=p.get_page(page)
     if request.method=='POST':
         searched=request.POST['searched']
-        articles=Article.objects.filter(Q(title__contains=searched)|Q(category__slug__contains=searched)|Q(card_headline__contains=searched))
-        return render(request,'blog/blog.html',{'articles':articles,'categories':categories})
+        articlesp=Article.objects.filter(Q(title__contains=searched)|Q(category__slug__contains=searched)|Q(card_headline__contains=searched)|Q(tags__name__contains=searched)).distinct()
+        return render(request,'blog/blog.html',{'articlesp':articlesp,'categories':categories})
 
     else:    
         pass
